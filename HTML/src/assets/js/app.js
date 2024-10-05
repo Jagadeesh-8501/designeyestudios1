7526838eb2e8680f952f0951bdda53774a6ad872
Jagadeesh_8501 <nani08052001@gmail.com>
2024-10-05 13:44:18 +0530

    first commit

---
+++ HTML/src/assets/js/app.js
@@ -0,0 +1,13 @@
+/* Template Name: Floti - Tailwind CSS Authentication Pages Template
+  Author: Zoyothemes
+  Email: zoyothemes@gmail.com
+  Website: http://zoyothemes.com/
+  Version: 1.0.0
+  Created: February 2024
+  File: Main Css File
+*/
+
+/*================================
+ *      02.  lucide Js           *
+ ================================*/
+lucide.createIcons();
---
+++ script.js
@@ -0,0 +1,25 @@
+
+document.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var maxScroll = 300; // Adjust this value as needed
+    var opacity = Math.max(1 - scrollTop / maxScroll, 0);
+    
+    var div = document.getElementById('disappear-on-scroll');
+    div.style.opacity = opacity;
+
+    var hero = document.querySelector('.hero');
+    hero.style.opacity = opacity;
+});
+
+document.getElementById('nav_button').addEventListener('change', function() {
+    var nav = document.getElementById('nav1');
+    if (this.checked) {
+        nav.classList.add('open');
+        document.body.classList.add('no-scroll');
+        nav.classList.remove('close');
+    } else {
+        nav.classList.remove('open');
+        nav.classList.add('close');
+        document.body.classList.remove('no-scroll');
+    }
+});
